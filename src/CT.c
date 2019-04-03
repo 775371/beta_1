8b8394329f712d26eced55e6a97fea0e326e863a
775371 <44507768+775371@users.noreply.github.com>
2019-04-02 20:54:10 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -541,7 +541,7 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
                 Rprintf("temp in cat in CT.c %d.\n", temp); 
                 Rprintf("best in cat in CT.c %d.\n", best); 
                
-      if (temp > best) {
+      if (best<= temp) {
 		    Rprintf("YES!cat: compare temp and best\n");
                     best = temp;
 				  
